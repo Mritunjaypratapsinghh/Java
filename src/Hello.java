f56618f5de2e1768718b89d89fb57a839577d802
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-02-11 15:18:36 +0530

    basic programs

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,9 @@ public class Hello {// this is a block
 
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println(4+4);
+        System.out.println("4+4");
+        System.out.println(4*4);
     }
 
 }
