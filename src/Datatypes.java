cadcea476d7a6405a7f5c8ba0422e817cdbf79d3
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-02-11 17:08:55 +0530

    Primitive Datatypes

---
+++ src/Datatypes.java
@@ -0,0 +1,19 @@
+public class Datatypes {
+
+    public static void main(String[] args){
+
+        boolean isPassed = true;// By default stores false if not assigned
+        byte marks = 127; // RANGE -128 to 127
+        short num = 1234; // RANGE -32768 to 32767
+        int count = 10; // RANGE -2^31 to 2^31
+        long big = 2345678; // To Store very large number
+        float pi = 3.14f; // for storing 6 to 7 decimal digits and have to use f explicitly
+        double secondPi = 3.143456789567; // for precision of decimal numbers, can handle 15 decimal digits
+        char myLetter = 'A'; //ony contain single character withing single quotes
+
+
+        System.out.println(pi);
+        System.out.println(secondPi);
+        System.out.println(myLetter);
+    }
+}
