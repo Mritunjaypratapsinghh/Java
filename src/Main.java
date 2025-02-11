6a1f24d067da20a5f71193f8ccd0ac76e9ee4eee
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-02-11 16:12:35 +0530

    Variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        int age = 23;
+        int AGE = 30;
+        int _AGE = 24;
+        int $AGE = 24;
+        int myAge = 25;
+        int age_1 =2;
+
+        System.out.println("age:"+age+" AGE:"+AGE+" $AGE:"+$AGE+" myAge:"+myAge+" age_1:"+age_1);
+
+
     }
 }
