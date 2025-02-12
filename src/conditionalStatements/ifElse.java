9044f99d4621cd71fd60c79a6955b0a71c4c4cd0
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-02-12 19:09:43 +0530

    Simple Conditional If else

--- src/conditionalStatements/ifElse.java
+++ src/conditionalStatements/ifElse.java
@@ -1,4 +1,25 @@
 package conditionalStatements;
-
+import java.util.Scanner;
 public class ifElse {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your Age:");
+        int voterAge = input.nextInt();
+
+        if(voterAge>18){
+            System.out.println("You are eligible for Voting");
+        }
+        else if(voterAge==18){
+            System.out.println("Eligible");
+        }
+        else{
+            System.out.println("Not Eligible, Age is less than 18");
+        }
+        System.out.println("Simple if-else if -else");
+
+
+
+
+
+    }
 }
