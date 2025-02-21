68ff1703fb8396a426cca0d4f3dd4f196b57e046
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-02-21 15:35:41 +0530

    Generic Method and Bounded Generic Method

--- src/genericsAndWrapperClasses/GenericMethods.java
+++ src/genericsAndWrapperClasses/GenericMethods.java
@@ -1,4 +1,33 @@
 package genericsAndWrapperClasses;
 
 public class GenericMethods {
+    public static void main(String[] args) {
+        printData("hello");  // Calling generic static method
+        printData(12);
+
+        GenericMethods obj = new GenericMethods();
+        obj.doubleData("Any data");  // Calling generic instance method
+        obj.doubleData(121);
+
+        CustomClass.doubleData(6);  // Calling static generic method from CustomClass
+    }
+
+    // Generic static method to print any type of data
+    static <E> void printData(E data) {
+        System.out.println(data);
+    }
+
+    // Generic instance method to print any type of data
+    <E> void doubleData(E data) {
+        System.out.println(data);
+    }
+}
+
+// Custom class with a bounded generic method
+class CustomClass {
+    // Bounded generic static method that only accepts Numbers
+    // This ensures that only Number types (Integer, Double, etc.) can be passed
+    public static <E extends Number> void doubleData(E data) {
+        System.out.println(data);
+    }
 }
